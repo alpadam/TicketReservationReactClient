fda5e30668f43409e64bb8e6b967251ee12f0c1c
alpadam <laszloadam1994@gmail.com>
2016-11-01 13:29:01 +0100

    Initial Router settings

--- client/client.js
+++ client/client.js
@@ -3,6 +3,11 @@ import { render } from 'react-dom';
 import App from '../components/App';
 import configureStore from '../redux/store';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+
+import TodoInput from '../components/TodoInput';
+import TodoList from '../components/TodoList';
+
 //es 5
 //var React = require('react');
 //var render = require('react-dom').render;
@@ -23,7 +28,12 @@ let store = configureStore(initialState);
 
 render(
   <Provider store={store}>
-    <App/>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={TodoList}  />
+        <Route path="input" component={TodoInput} />
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('app')
 );
--- components/App.js
+++ components/App.js
@@ -3,30 +3,15 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import actions from '../redux/actions';
 
-import TodoInput from './TodoInput';
-import TodoList from './TodoList';
-import UserInfo from './UserInfo';
-
-//es6
-/*class App extends Component {
-  render(){
-    return <div>This is a REACT app now...</div>;
-  };
-};*/
-
-//es5
-var App = React.createClass({
+class App extends Component {
   render(){
     return (
-        <div>
-          <h1>Todo list</h1>
-          <UserInfo user={this.props.user} createNewUserId={this.props.actions.createNewUserId}/>
-          <TodoInput actions={this.props.actions} />
-          <TodoList todos={this.props.todos} actions={this.props.actions}/>
-        </div>
+      <div>
+        {React.cloneElement(this.props.children, this.props)}
+      </div>
     )
   }
-});
+};
 
 //azért, hogy ne kelljen minden egyes modulhoz hozzárendelni/átadni
 function mapStateToProps(state){
